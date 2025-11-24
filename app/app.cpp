7996a89bde891d980e2bdabce771c1cd4d84eaf9
petar puharić <puharicpetar103@gmail.com>
2025-11-24 20:34:19 +0100

    Prvi commit

--- app/app.cpp
+++ app/app.cpp
@@ -3,5 +3,28 @@
 
 namespace vsite::oop::v5
 {
+	tire::tire(double diameter) : diameter_(diameter) {}
+	double tire::diameter() const {
+		return diameter_;
+	}
 
+
+	vehicle::vehicle(int year) : year_(year) {}
+	int vehicle::year() const {
+		return year_;
+	}
+
+
+	product::product(double price) : price_(price) {}
+	double product::price() const {
+		return price_;
+	}
+
+
+	car::car(int year, double price, double tire_diameter)
+		: vehicle(year), product(price), tire_(tire_diameter) {
+	}
+	double car::tire_diameter() const {
+		return tire_;
+	}
 }
--- app/app.h
+++ app/app.h
@@ -2,7 +2,46 @@
 #include <string>
 #include <memory>
 
-namespace vsite::oop::v5 
-{
+namespace vsite::oop::v5 {
+	constexpr double operator"" _inch(unsigned long long val) {
+		return val * 2.54;
+	}
 
+	constexpr double operator"" _eur(unsigned long long val) {
+		return val * 7.5345;
+	}
+	
+	class tire {
+	public:
+		tire(double diameter);
+		double diameter() const;
+	private:
+		double diameter_;
+	};
+
+	class vehicle {
+	public:
+		vehicle(int year);
+		int year() const;
+	private:
+		int year_;
+	};
+	
+	class product {
+	public:
+		product(double price);
+		double price() const;
+	private:
+		double price_;
+	};
+
+	
+
+	class car : public vehicle, public product {
+	public:
+		car(int year, double price, double tire_diameter);
+		double tire_diameter() const;
+	private:
+		double tire_;
+	};
 }
